df6940f6e6d6fe927bc2e5a7b314f320439e21f2
Khushi Sarkari <ksarkari@ex2india.com>
2020-12-30 12:26:04 +0530

    mevn setup

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Item from "./views/Item.vue";
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: () => import("./views/About.vue")
+    },
+    {
+      path: "/item",
+      name: "item",
+      component: Item
     }
   ]
 });
